f7ba9cbce363441fa940b8b23a4ba5ba24fca36d
pemiinem <pedro.m.rodiguero@gmail.com>
2021-07-30 20:44:52 -0300

    Final version

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,53 @@
-
-import org.jetbrains.annotations.NotNull;
-
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class main {
+public class Main {
+
+    private static final String[] names = {"Lucas", "Pedro", "Rafael", "Natalia", "Rodrigo",
+            "Daniel", "Matheus", "Carol", "Karina", "Jackson"};
+
+    private static final String[] telephones = {"1234565", "33212", "3213", "354", "35645",
+            "54335", "5435434", "456546", "555464", "54565"};
+
+    private static final String[] address = {"Brasilia", "Rio", "Asa Norte", "New York", "Asa Norte",
+            "Salvador", "Sao Paulo", "Goiania", "Asa Sul", "Gama"};
+
+    private static final String[] productsName = {"Water", "Juice", "Soda", "Popcorn", "Candy", "Chocolate",
+            "Ice cream", "Sandwich", "Hot dog", "Hamburger"};
+
+    private static final String[] description = {"Water", "Juice", "Soda", "Popcorn", "Candy", "Chocolate",
+            "Ice cream", "Sandwich", "Hot dog", "Hamburger"};
+
+    private static final Double[] price = {2.0, 3.0, 3.5, 5.0, 6.0, 4.5, 8.0, 10.0, 9.0, 11.0 };
+
+    private static final int[] quantity = {10, 8, 15, 20, 6, 9, 25, 30, 4, 8};
+
+    private static final Double[] profit = {1.5, 2.5, 3.0, 3.5, 8.0, 4.5, 5.0, 6.5, 10.5, 10.0};
 
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         ArrayList<Client> clients = new ArrayList<>();
         ArrayList<Product> products = new ArrayList<>();
+
+        for(int i = 0; i < 10; i++){
+            Client client = new Client();
+
+            client.name = names[i];
+            client.address = address[i];
+            client.phone = telephones[i];
+            clients.add(client);
+        }
+
+        for(int i = 0; i < 10; i++){
+            Product product = new Product();
+
+            product.name = productsName[i];
+            product.profit = profit[i];
+            product.quantity = quantity[i];
+            product.description = description[i];
+            product.price = price[i];
+            products.add(product);
+        }
 
         int option;
 
@@ -17,6 +55,7 @@ public class main {
             menu();
             option = sc.nextInt();
             sc.nextLine();
+
             switch (option) {
                 case 1 -> newClient(clients);                           //New client
 
@@ -26,15 +65,14 @@ public class main {
 
                 case 4 -> searchProduct(products);                      //Search product
 
-                case 5 -> {
-                    System.out.println("Case 5");                       //New sale
+                case 5 -> {                                              //New sale
                     for(Client clientOfArray: clients){                 //Printing list of clients
                         System.out.println("Name: " + clientOfArray.name);
                         System.out.println("Address: " + clientOfArray.address);
                         System.out.println("Phone: " + clientOfArray.phone);
                         System.out.println("-------------------");
                     }
-                    System.out.print("Select one of the clients above: ");
+                    System.out.print("Select one of the clients above: \n Name: ");
                     String clientSelected = sc.nextLine();
 
                     for(Product productOfArray: products){              //Printing list of products
@@ -43,7 +81,7 @@ public class main {
                         System.out.println("Stock: " + productOfArray.quantity);
                         System.out.println("-------------------");
                     }
-                    System.out.print("Select one of the products above: ");
+                    System.out.print("Select one of the products above: \n Name: ");
                     String productSelected = sc.nextLine();
                     System.out.print("How many items? ");
                     int amountSelected = sc.nextInt();
@@ -81,7 +119,7 @@ public class main {
         System.out.print("Choose one of the above: ");
     }
 
-    static void newClient(@NotNull ArrayList<Client> clients) {
+    static void newClient(ArrayList<Client> clients) {
         Scanner sc = new Scanner(System.in);
         Client client = new Client();                     //New client
         System.out.print("Client's name: ");
@@ -98,10 +136,11 @@ public class main {
             System.out.println(clientOfArray.name);
             System.out.println(clientOfArray.address);
             System.out.println(clientOfArray.phone);
+            System.out.println("---------------------");
         }
     }
 
-    static void newProduct(@NotNull ArrayList<Product> products){
+    static void newProduct(ArrayList<Product> products){
         Scanner sc = new Scanner(System.in);
         Product product = new Product();
         System.out.print("Product: ");
@@ -117,7 +156,7 @@ public class main {
         products.add(product);
     }
 
-    static void searchClient(@NotNull ArrayList<Client> clients){
+    static void searchClient(ArrayList<Client> clients){
         String searchName;
         Scanner sc = new Scanner(System.in);
         System.out.print("Name: ");
@@ -133,21 +172,18 @@ public class main {
                 if ((answer.equalsIgnoreCase("y"))) {
                     System.out.print("New name: ");
                     String newName = sc.nextLine();
-                    System.out.print("Old name: " + arrayOfClients.name + "\nNew name: " + newName);
+                    System.out.println("\nOld name: " + arrayOfClients.name + "\nNew name: " + newName);
                     arrayOfClients.name = newName;
-                    System.out.println("New name!!! " + arrayOfClients.name);
 
                     System.out.print("New address: ");
                     String newAddress = sc.nextLine();
-                    System.out.print("Old address: " + arrayOfClients.address + "\nNew address: " + newAddress);
+                    System.out.println("Old address: " + arrayOfClients.address + "\nNew address: " + newAddress);
                     arrayOfClients.address = newAddress;
-                    System.out.println("New address!!! " + arrayOfClients.address);
 
                     System.out.print("New phone: ");
                     String newPhone = sc.nextLine();
-                    System.out.print("Old phone: " + arrayOfClients.phone + "\nNew phone: " + newPhone);
+                    System.out.println("Old phone: " + arrayOfClients.phone + "\nNew phone: " + newPhone);
                     arrayOfClients.phone = newPhone;
-                    System.out.println("New phone!!! " + arrayOfClients.phone);
                 }
             } else {
                 System.out.println("Client not found!!");
@@ -155,7 +191,7 @@ public class main {
         }
     }
 
-    static void searchProduct(@NotNull ArrayList<Product> products){
+    static void searchProduct(ArrayList<Product> products){
         Scanner sc = new Scanner(System.in);
         String searchProduct;
         System.out.print("Name: ");
@@ -235,7 +271,7 @@ public class main {
 //           }
 //       }
 
-    static void inventory(@NotNull ArrayList<Product> products){
+    static void inventory(ArrayList<Product> products){
         for(Product productOfArray: products){
             System.out.println("Name: " + productOfArray.name);
             System.out.println("Price: " + productOfArray.price);
